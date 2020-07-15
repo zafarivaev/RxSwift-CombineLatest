710060b3139f7cb771ed31e83176eac3ac28400d
zafarivaev <z.ivaev@mail.ru>
2020-07-15 20:27:55 +0500

    Initial Commit

---
+++ CombineLatest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CombineLatest
+//
+//  Created by Zafar on 7/15/20.
+//  Copyright © 2020 Zafar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
